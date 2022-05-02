e9b3c90ab757e54787a91566bb3130705710a296
Riley Sherlock <riley.sherlock@mts.net>
2022-05-02 13:38:36 -0500

    outline drawing of palced ships

--- src/DisplayBoard.js
+++ src/DisplayBoard.js
@@ -1,10 +1,14 @@
+import ShipNames from './ShipNames';
+
 export default class DisplayBoard {
-  constructor(container) {
+  constructor(container, board) {
     let div = document.createElement('div');
     div.classList.add('board');
+
     for (let y = 0; y < 11; y++) {
       for (let x = 0; x < 11; x++) {
         let cell = document.createElement('div');
+        cell.id = JSON.stringify({ x: x - 1, y: y - 1 });
         if (y === 0 && x > 0) {
           cell.textContent = String.fromCharCode(64 + x);
           cell.classList.add('labelCell');
@@ -12,8 +16,7 @@ export default class DisplayBoard {
           cell.textContent = y.toString();
           cell.classList.add('labelCell');
         } else if (x > 0 && y > 0) cell.classList.add('cell');
-        cell.ondragover = (e) => e.preventDefault();
-        cell.ondrop = (e) => {
+        cell.ondragover = (e) => {
           e.preventDefault();
           let info = JSON.parse(e.dataTransfer.getData('text'));
           let origin = document.elementFromPoint(
@@ -21,13 +24,44 @@ export default class DisplayBoard {
             e.y - e.offsetY - info.offsetY + 25
           );
           if (origin.classList.contains('cell') && div.contains(origin)) {
-            cell.style.backgroundColor = 'red';
-            origin.style.backgroundColor = 'blue';
+            let originCoords = JSON.parse(origin.id);
+            board.removeShip(info.id);
+            board.placeShip(info.id, originCoords.x, originCoords.y, false);
+            // draw();
+            // remove ship
+            // place ship
+            // draw
+            // console.log(originCoords);
+            // cell.style.backgroundColor = 'red';
+            // origin.style.backgroundColor = 'blue';
           }
         };
+        // cell.ondrop = (e) => {
+        //   e.preventDefault();
+        //   let info = JSON.parse(e.dataTransfer.getData('text'));
+        //   let origin = document.elementFromPoint(
+        //     e.x - e.offsetX - info.offsetX + 25,
+        //     e.y - e.offsetY - info.offsetY + 25
+        //   );
+        //   if (origin.classList.contains('cell') && div.contains(origin)) {
+        //     let originCoords = JSON.parse(origin.id);
+        //     console.log(originCoords);
+        //     cell.style.backgroundColor = 'red';
+        //     origin.style.backgroundColor = 'blue';
+        //   }
+        // };
         div.appendChild(cell);
       }
     }
     container.appendChild(div);
+  }
+
+  draw() {
+    for (const name in ShipNames) {
+      // get info (origin, horizontal, sunk)
+      // hide all cells where ship is to be placed?
+      // add shipyard element to grid div
+      //
+    }
   }
 }
