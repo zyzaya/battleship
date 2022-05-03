d1d00eaee6be1718d42201966652ec7d10f70840
Riley Sherlock <riley.sherlock@mts.net>
2022-05-03 14:58:38 -0500

    polish DisplayBoard

--- src/DisplayBoard.js
+++ src/DisplayBoard.js
@@ -1,7 +1,7 @@
-import Display from './Display';
-import ShipNames from './ShipNames';
-
 export default function DisplayBoard(container) {
+  let div = document.createElement('div');
+  div.classList.add('board');
+
   let displayBoard = {};
   displayBoard.onCellDrop = {};
   displayBoard.onCellClick = {};
@@ -11,8 +11,7 @@ export default function DisplayBoard(container) {
       return JSON.parse(cell.id);
     else return undefined;
   };
-  let div = document.createElement('div');
-  div.classList.add('board');
+
   displayBoard.drawShip = function (name, info) {
     if (info != undefined) {
       let ship = document.getElementById(name);
@@ -36,27 +35,31 @@ export default function DisplayBoard(container) {
     }
   };
 
-  for (let y = 0; y < 11; y++) {
-    for (let x = 0; x < 11; x++) {
-      let cell = document.createElement('div');
-      cell.id = JSON.stringify({ x: x - 1, y: y - 1 });
-      cell.style.gridArea = `${y + 1} / ${x + 1} / ${y + 2} / ${x + 2}`;
-      if (y === 0 && x > 0) {
-        cell.textContent = String.fromCharCode(64 + x);
-        cell.classList.add('labelCell');
-      } else if (x === 0 && y > 0) {
-        cell.textContent = y.toString();
-        cell.classList.add('labelCell');
-      } else if (x > 0 && y > 0) cell.classList.add('cell');
+  let setupCells = function () {
+    for (let y = 0; y < 11; y++) {
+      for (let x = 0; x < 11; x++) {
+        let cell = document.createElement('div');
+        cell.id = JSON.stringify({ x: x - 1, y: y - 1 });
+        cell.style.gridArea = `${y + 1} / ${x + 1} / ${y + 2} / ${x + 2}`;
+        if (y === 0 && x > 0) {
+          cell.textContent = String.fromCharCode(64 + x);
+          cell.classList.add('labelCell');
+        } else if (x === 0 && y > 0) {
+          cell.textContent = y.toString();
+          cell.classList.add('labelCell');
+        } else if (x > 0 && y > 0) cell.classList.add('cell');
 
-      cell.ondragover = (e) => e.preventDefault();
-      cell.ondrop = (e) => {
-        if (displayBoard.onCellDrop instanceof Function)
-          displayBoard.onCellDrop(e, x + 1, y + 1);
-      };
-      div.appendChild(cell);
+        cell.ondragover = (e) => e.preventDefault();
+        cell.ondrop = (e) => {
+          if (displayBoard.onCellDrop instanceof Function)
+            displayBoard.onCellDrop(e, x + 1, y + 1);
+        };
+        div.appendChild(cell);
+      }
     }
-  }
-  container.appendChild(div);
+    container.appendChild(div);
+  };
+
+  setupCells();
   return displayBoard;
 }
