7af448785861457da9e4d122eab6462b88df8e20
Riley Sherlock <riley.sherlock@mts.net>
2022-05-09 14:30:41 -0500

    outline Battleship turns

--- src/Battleship.js
+++ src/Battleship.js
@@ -4,6 +4,7 @@ import ShipNames from './ShipNames';
 export default function Battleship(player1, player2) {
   let p1Board = Board(10);
   let p2Board = Board(10);
+  let isP1Turn = true;
 
   let placeShip = function (name, x, y, horizontal, isPlayer1) {
     if (isPlayer1 && p1Board.isValidShipPlacement(name, x, y, horizontal))
@@ -16,7 +17,22 @@ export default function Battleship(player1, player2) {
     return isPlayer1 ? p1Board.getShipInfo(name) : p2Board.getShipInfo(name);
   };
 
-  let start = function () {};
+  let nextTurn = function () {
+    // let guess = isP1Turn ? player1.getGuess() : player2.getGuess();
+    // throw error if invalid guess
+    // p1Board.hit(guess.x, guess.y);
+    // draw();
+    // if (isWinner()) endGame();
+    // else {
+    //   isP1Turn = !isP1Turn;
+    //   nextTurn();
+    // }
+  };
+
+  let start = function () {
+    isP1Turn = true;
+    nextTurn();
+  };
 
   return { placeShip, getShipInfo };
 }
