7448e1e3b924cc6e24ae3ac9c5de85c2e48d6e1b
Riley Sherlock <riley.sherlock@mts.net>
2022-05-23 13:22:34 -0500

    reorganize Display setup into functions

--- src/Display.js
+++ src/Display.js
@@ -5,81 +5,92 @@ import Shipyard from './Shipyard';
 export default function Display(battleship, player, opponent, container) {
   let opponentDisplay = DisplayBoard(container, 'opponent');
   let playerDisplay = DisplayBoard(container, 'player');
+  let startDiv = document.createElement('div');
+  let start = document.createElement('button');
 
   let isAllShipsPlaced = function () {
     return Object.keys(ShipNames).every(
       (name) => battleship.getShipInfo(name, true) !== undefined
     );
   };
-  playerDisplay.onCellDrop = (e) => {
-    e.preventDefault();
-    let info = JSON.parse(e.dataTransfer.getData('text'));
-    let origin = playerDisplay.cellFromPoint(
-      e.x - e.offsetX - info.offsetX + 25,
-      e.y - e.offsetY - info.offsetY + 25
-    );
-    if (origin !== undefined)
-      battleship.placeShip(
+
+  let setupShipPlacement = function () {
+    playerDisplay.onCellDrop = (e) => {
+      e.preventDefault();
+      let info = JSON.parse(e.dataTransfer.getData('text'));
+      let origin = playerDisplay.cellFromPoint(
+        e.x - e.offsetX - info.offsetX + 25,
+        e.y - e.offsetY - info.offsetY + 25
+      );
+      if (origin !== undefined)
+        battleship.placeShip(
+          info.name,
+          origin.x,
+          origin.y,
+          info.horizontal,
+          true
+        );
+      playerDisplay.drawShip(
         info.name,
-        origin.x,
-        origin.y,
-        info.horizontal,
+        battleship.getShipInfo(info.name, true)
+      );
+      if (isAllShipsPlaced()) start.disabled = false;
+    };
+  };
+  let setupGuessing = function () {
+    opponentDisplay.onCellClick = (e) => {
+      let origin = opponentDisplay.cellFromPoint(
+        e.x - e.offsetX,
+        e.y - e.offsetY
+      );
+      player.setGuess(origin.x, origin.y);
+    };
+  };
+
+  let setupShipyard = function () {
+    let shipyard = Shipyard(container, 'player');
+    shipyard.onShipDragStart = (e, name) => {
+      let info = battleship.getShipInfo(name, true);
+      let horizontal = info === undefined ? false : info.horizontal;
+      e.dataTransfer.setData(
+        'text',
+        JSON.stringify({
+          name: name,
+          offsetX: e.offsetX,
+          offsetY: e.offsetY,
+          horizontal: horizontal,
+        })
+      );
+    };
+    shipyard.onShipClick = (e, name) => {
+      let info = battleship.getShipInfo(name, true);
+      if (info === undefined) return;
+      battleship.placeShip(
+        name,
+        info.origin.x,
+        info.origin.y,
+        !info.horizontal,
         true
       );
-    playerDisplay.drawShip(info.name, battleship.getShipInfo(info.name, true));
-    if (isAllShipsPlaced()) start.disabled = false;
-  };
-  opponentDisplay.onCellClick = (e) => {
-    let origin = opponentDisplay.cellFromPoint(
-      e.x - e.offsetX,
-      e.y - e.offsetY
-    );
-    player.setGuess(origin.x, origin.y);
+      playerDisplay.drawShip(name, battleship.getShipInfo(name, true));
+    };
   };
 
-  Shipyard(container, 'opponent');
-  let shipyard = Shipyard(container, 'player');
-  shipyard.onShipDragStart = (e, name) => {
-    let info = battleship.getShipInfo(name, true);
-    let horizontal = info === undefined ? false : info.horizontal;
-    e.dataTransfer.setData(
-      'text',
-      JSON.stringify({
-        name: name,
-        offsetX: e.offsetX,
-        offsetY: e.offsetY,
-        horizontal: horizontal,
-      })
-    );
+  let setupStart = function () {
+    start.classList.add('start');
+    start.disabled = true;
+    start.textContent = 'Start Game';
+    start.onclick = () => {
+      for (const name in ShipNames) {
+        let pos = opponent.getShipPosition(name);
+        battleship.placeShip(name, pos.x, pos.y, pos.horizontal, false);
+        opponentDisplay.drawShip(name, battleship.getShipInfo(name, false));
+      }
+      battleship.start();
+    };
+    startDiv.appendChild(start);
+    container.appendChild(startDiv);
   };
-  shipyard.onShipClick = (e, name) => {
-    let info = battleship.getShipInfo(name, true);
-    if (info === undefined) return;
-    battleship.placeShip(
-      name,
-      info.origin.x,
-      info.origin.y,
-      !info.horizontal,
-      true
-    );
-    playerDisplay.drawShip(name, battleship.getShipInfo(name, true));
-  };
-
-  let startDiv = document.createElement('div');
-  let start = document.createElement('button');
-  start.classList.add('start');
-  start.disabled = true;
-  start.textContent = 'Start Game';
-  start.onclick = () => {
-    for (const name in ShipNames) {
-      let pos = opponent.getShipPosition(name);
-      battleship.placeShip(name, pos.x, pos.y, pos.horizontal, false);
-      opponentDisplay.drawShip(name, battleship.getShipInfo(name, false));
-    }
-    battleship.start();
-  };
-  startDiv.appendChild(start);
-  container.appendChild(startDiv);
 
   let drawShips = function () {
     for (const name in ShipNames) {
@@ -100,9 +111,16 @@ export default function Display(battleship, player, opponent, container) {
   let draw = function () {
     drawShips();
     drawGuesses();
-    // for each placed ship draw ship on player board
-    // for each player guess draw x or o on opponent board
-    // for each opponent guess draw x or o on player board
   };
+
+  let setup = function () {
+    setupShipPlacement();
+    setupGuessing();
+    setupShipyard();
+    Shipyard(container, 'opponent');
+    setupStart();
+  };
+
+  setup();
   return { draw };
 }
